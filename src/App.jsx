811ffa244aa6c0dc56ac6e3ed1070d0f373d7804
Taarik Ashenafi <147209483+taarikashenafi@users.noreply.github.com>
2024-11-07 21:48:16 -0600

    added app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Dashboard from './components/Dashboard'
+import AuthPage from './components/AuthPage'
+import Mission from './components/Mission'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
-      <About />
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   )
 }
 
